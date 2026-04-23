d8a556887146a1bc88995e6cfd039cf11aeb8624
Rusich <vasenin@verqor.com>
2026-04-23 22:47:32 +0300

    added logout

--- shop/app/middleware/auth.ts
+++ shop/app/middleware/auth.ts
@@ -3,7 +3,7 @@ import type {LoginResponse} from "~/types/auth.types";
 export default defineNuxtRouteMiddleware((from, to) => {
     const auth = useCookie<LoginResponse>('auth');
 
-    if (!auth.value) {
+    if (!auth.value || !auth.value.token) {
         return navigateTo('/auth')
     }
 });
--- shop/app/pages/account/index.vue
+++ shop/app/pages/account/index.vue
@@ -1,11 +1,18 @@
 <script setup lang="ts">
   definePageMeta({
     middleware: 'auth',
-  })
+  });
+
+  const store = useAuthStore();
 </script>
 
 <template>
-<div class="account">Account</div>
+<div class="account">
+  <h1>Аккаунт</h1>
+  <p>
+    <NuxtLink @click="store.logout" to="/auth">Выход</NuxtLink>
+  </p>
+</div>
 </template>
 
 <style scoped>
